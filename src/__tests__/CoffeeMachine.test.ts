f7aa34d7fe55debf0696407d88e3101e2af54033
OLIMINATOR <oliverplassiard@proton.me>
2025-09-30 08:44:14 +0200

    use prettier

--- src/__tests__/CoffeeMachine.test.ts
+++ src/__tests__/CoffeeMachine.test.ts
@@ -1,12 +1,14 @@
-import { Drink } from '../Drink';
-import { CoffeeMachine } from '../CoffeeMachine';
+import { Drink } from "../Drink";
+import { CoffeeMachine } from "../CoffeeMachine";
 
 describe("CoffeeMachine", () => {
-  it("should serve coffee if exact money inserted", () => {
-    const machine = new CoffeeMachine();
+    it("should serve coffee if exact money inserted", () => {
+        const machine = new CoffeeMachine();
 
-    const drink = new Drink("Coffee", 2, false, 0, "small");
+        const drink = new Drink("Coffee", 2, false, 0, "small");
 
-    expect(machine.serve(drink, 2, false, 10)).toBe("Serving Coffee (small)");
-  });
+        expect(machine.serve(drink, 2, false, 10)).toBe(
+            "Serving Coffee (small)"
+        );
+    });
 });
